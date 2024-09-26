d48dda40b2b6e5f5260a81acfd7af7d4822e7aba
David Lazaro Fernandez <davidlazaro-fernandez@blend.com>
2024-09-26 01:17:02 -0600

    Deleted template

--- app/components/Contact/index.tsx
+++
@@ -1,9 +0,0 @@
-function Contact() {
-    return (
-        <div className='flex justify-between items-center bg-transparent font-Bai Jamjuree py-8 px-6 mx-12 max-h-32 border-2 border-t-0 border-deepGray uppercase bg-[#1b1b1b] text-white'>
-            <p><span className="text-deepGray">contacto: </span>hello@kaix.com</p>
-            <p>comienza tu camino como inversionista</p>
-        </div>
-    );
-}
-export default Contact;
