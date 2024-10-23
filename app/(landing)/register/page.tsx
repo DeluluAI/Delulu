3ab68e721e401738b8ffda5d0b5f81ad67a67121
David Lazaro Fernandez <davidlazaro-fernandez@blend.com>
2024-10-23 10:48:36 -0600

    [HOTFIX] Build hotfixes

--- app/(landing)/register/page.tsx
+++ app/(landing)/register/page.tsx
@@ -1,9 +1,8 @@
-import { CompanySignUpForm } from "@/components/landing/multi-step-form-demo"
 
 export default function RegisterPage() {
     return (
         <div className="max-w-3xl mx-auto">
-            <CompanySignUpForm />
+            TEST
         </div>
     )
 }
